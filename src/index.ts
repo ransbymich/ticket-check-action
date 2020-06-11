928fccd50c7647fb6f40feefc104f3411225841b
ransbymich <38529949+ransbymich@users.noreply.github.com>
2020-06-11 10:28:30 -0400

    inputs are more valid this time

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,8 @@ async function run(): Promise<void> {
   try {
     // Check for a ticket reference in the title
     const title: string = context?.payload?.pull_request?.title;
-    const titleRegexBase = getInput('titleRegex', { required: true });
-    const titleRegexFlags = getInput('titleRegexFlags', {
-      required: true
-    });
-    const titleRegex = new RegExp(titleRegexBase, titleRegexFlags);
-    const titleCheck = title.match(titleRegex);
 
     debug(title);
-
-    // Return and approve if the title includes a Ticket ID
-    if (titleCheck !== null) {
-      debug('Title includes a ticket ID');
-
-      return;
-    }
 
     // Instantiate a GitHub Client instance
     const token = getInput('token', { required: true });
